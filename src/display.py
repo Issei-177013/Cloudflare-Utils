e0d0f2706b119ef408e8489d53056cac3b303479
Issei-177013 <116876441+Issei-177013@users.noreply.github.com>
2025-07-15 13:02:16 +0330

    feat: Display zone count in account list and use tabulate to show tables

--- src/display.py
+++ src/display.py
@@ -1,6 +1,8 @@
+from tabulate import tabulate
+
 def display_as_table(data, headers):
     """
-    Displays a list of dictionaries as a formatted table.
+    Displays a list of dictionaries as a formatted table using the tabulate library.
 
     :param data: A list of dictionaries.
     :param headers: A list of keys to display as columns.
@@ -9,21 +11,5 @@ def display_as_table(data, headers):
         print("No data to display.")
         return
 
-    # Calculate column widths
-    column_widths = {header: len(header) for header in headers}
-    for row in data:
-        for header in headers:
-            column_widths[header] = max(column_widths[header], len(str(row.get(header, ""))))
-
-    # Print header
-    header_line = " | ".join(f"{header:<{column_widths[header]}}" for header in headers)
-    print(f"+-{'--+-'.join('-' * column_widths[header] for header in headers)}-+")
-    print(f"| {header_line} |")
-    print(f"+-{'--+-'.join('-' * column_widths[header] for header in headers)}-+")
-
-    # Print data
-    for row in data:
-        row_line = " | ".join(f"{str(row.get(header, '')):<{column_widths[header]}}" for header in headers)
-        print(f"| {row_line} |")
-
-    print(f"+-{'--+-'.join('-' * column_widths[header] for header in headers)}-+")
+    # The tabulate function can directly take a list of dictionaries and headers.
+    print(tabulate(data, headers=headers, tablefmt="grid"))
